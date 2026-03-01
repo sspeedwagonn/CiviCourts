c4d8e477a40f104e2357cbe7b17916a14f0d8e42
sspeedwagonn <burnerforcarter@gmail.com>
2026-02-28 22:06:49 -0800

    Working on warrant check

--- src/main/java/net/civicraft/civiCourts/command/subcommand/WarrantCheck.java
+++ src/main/java/net/civicraft/civiCourts/command/subcommand/WarrantCheck.java
@@ -3,6 +3,8 @@ package net.civicraft.civiCourts.command.subcommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Optional;
 import co.aikar.commands.annotation.Subcommand;
+import net.civicraft.civiCourts.manager.WarrantManager;
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 
 /*
@@ -14,6 +16,11 @@ Check if there is an active warrant against a player
 public class WarrantCheck {
     @Subcommand("check")
     public void onWarrantCheck(Player player, @Optional Player target) {
+        if (target != null && WarrantManager.hasActiveWarrant(target)) {
+            player.sendMessage(Component.text(target.displayName() + " has a warrant."));
+        } else if (WarrantManager.hasActiveWarrant(player)) {
+            player.sendMessage(Component.text("You have a warrant <info>."));
+        }
 
     }
 }
--- src/main/java/net/civicraft/civiCourts/manager/WarrantManager.java
+++ src/main/java/net/civicraft/civiCourts/manager/WarrantManager.java
@@ -1,9 +1,15 @@
 package net.civicraft.civiCourts.manager;
 
 import net.civicraft.civiCourts.object.Warrant;
+import org.bukkit.entity.Player;
 
 public class WarrantManager {
     public static void registerWarrant(Warrant warrant) {
 
+    }
+
+    public static boolean hasActiveWarrant(Player player) {
+        //check if a player has an active warrant
+        return true;
     }
 }
