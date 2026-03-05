7465268836caa1be03a5987c6bc1653cd5ec920a
sspeedwagonn <burnerforcarter@gmail.com>
2026-03-04 20:58:25 -0800

    Evidence & evidence manager

--- src/main/java/net/civicraft/civiCourts/command/subcommand/EvidenceList.java
+++ src/main/java/net/civicraft/civiCourts/command/subcommand/EvidenceList.java
@@ -3,8 +3,12 @@ package net.civicraft.civiCourts.command.subcommand;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Subcommand;
+import net.civicraft.civiCourts.manager.EvidenceManager;
 import net.civicraft.civiCourts.object.Case;
+import net.civicraft.civiCourts.object.Evidence;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
 
 /*
 /evidence list <case>
@@ -15,6 +19,6 @@ Lists evidence in a case with information about the evidence.
 public class EvidenceList extends BaseCommand {
     @Subcommand("list")
     public void onEvidenceList(Player player, Case c) {
-
+        ArrayList<Evidence> evidence = EvidenceManager.getEvidenceByCase(c);
     }
 }
---
+++ src/main/java/net/civicraft/civiCourts/manager/EvidenceManager.java
@@ -0,0 +1,12 @@
+package net.civicraft.civiCourts.manager;
+
+import net.civicraft.civiCourts.object.Case;
+import net.civicraft.civiCourts.object.Evidence;
+
+import java.util.ArrayList;
+
+public class EvidenceManager {
+    public static ArrayList<Evidence> getEvidenceByCase(Case c) {
+
+    }
+}
