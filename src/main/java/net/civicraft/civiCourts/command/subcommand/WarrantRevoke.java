d8ce70f041c07fe98e31617d052cda9ec15d832a
sspeedwagonn <burnerforcarter@gmail.com>
2026-03-07 00:12:42 -0800

    Warrant management

--- src/main/java/net/civicraft/civiCourts/command/subcommand/WarrantRevoke.java
+++ src/main/java/net/civicraft/civiCourts/command/subcommand/WarrantRevoke.java
@@ -1,7 +1,9 @@
 package net.civicraft.civiCourts.command.subcommand;
 
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Optional;
 import co.aikar.commands.annotation.Subcommand;
+import net.civicraft.civiCourts.manager.WarrantManager;
 import org.bukkit.entity.Player;
 
 /*
@@ -12,7 +14,12 @@ Revokes an active warrant by ID.
 @CommandAlias("warrant")
 public class WarrantRevoke {
     @Subcommand("revoke")
-    public void onWarrantRevoke(Player player, int warrantId) {
-
+    public void onWarrantRevoke(Player player, Player target, @Optional boolean all) {
+        if (all) {
+            WarrantManager.removeAllWarrants(target);
+        } else {
+            WarrantManager.removeWarrant(warrant);
+        }
+        //i need to think more about what im doing here
     }
 }
--- src/main/java/net/civicraft/civiCourts/manager/WarrantManager.java
+++ src/main/java/net/civicraft/civiCourts/manager/WarrantManager.java
@@ -1,6 +1,7 @@
 package net.civicraft.civiCourts.manager;
 
 import net.civicraft.civiCourts.object.Warrant;
+import net.civicraft.civiCourts.object.type.WarrantType;
 import org.bukkit.entity.Player;
 
 public class WarrantManager {
@@ -11,5 +12,22 @@ public class WarrantManager {
     public static boolean hasActiveWarrant(Player player) {
         //check if a player has an active warrant
         return true;
+    }
+
+    public static Warrant findWarrantByPlayer(Player player) {
+        return new Warrant(player, player, WarrantType.ARREST, "Filler");
+    }
+
+    public static boolean updateWarrant(Warrant warrant, boolean active) {
+        // update warrant status, reactivate or deactivate
+        return true;
+    }
+
+    public static void removeWarrant(Warrant warrant) {
+        // remove warrant entirely
+    }
+
+    public static void removeAllWarrants(Player player) {
+        //remove all warrants for a player
     }
 }
